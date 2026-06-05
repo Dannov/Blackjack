f1d187f657a6b1f1c7bd22e145ef16eff6c9fc86
Dannov <danielseleznov8@gmail.com>
2026-06-05 17:34:14 +0200

    changed somethings, fixed logic in checking players hand, removed time.sleep so you dont have to wait

--- Blackjack.py
+++ Blackjack.py
@@ -1,15 +1,12 @@
 import random
 import time
 
-# check pygame
-
 # do one dealer card secret as in the real game
 # optimize
 # fix minor bugs
 # fix logic
 
 print(f'welcome to black jack')
-time.sleep(1)
 
 def deck_refresh():
     return  {"2 ♡" : 2,"3 ♡" : 3,"4 ♡" : 4,"5 ♡" : 5,"6 ♡" : 6, "7 ♡" : 7, "8 ♡" : 8, "9 ♡" : 9, "10 ♡" : 10, "Jack ♡" : 10, "Queen ♡" : 10, "King ♡" : 10, "Ace ♡" : 11,
@@ -24,14 +21,12 @@ def draw_card(deck):
     card = random.choice(list(deck.keys()))
     point = deck[card]
     print(card)
-    time.sleep(1)
     del deck[card]
     return point
 
 def dealer_turn(deck):
     check_dealer = hand_refresh()
     print("dealer cards are")
-    time.sleep(1)
     card1 = draw_card(deck)
     card2 = draw_card(deck)
     check_dealer.extend([card1 , card2])
@@ -42,13 +37,11 @@ def dealer_turn(deck):
 def player_turn(deck):
     check_player = hand_refresh()
     print("your cards are")
-    time.sleep(1)
     card1 = draw_card(deck)
     card2 = draw_card(deck)
     check_player.extend([card1 , card2])
     all_points = card1 + card2
     print(f"your cards whole value = {all_points}")
-    time.sleep(1)
     return all_points , check_player
 
 def hit(deck):
@@ -61,16 +54,14 @@ def check_ace_dealer(dealer_hand, all_points):
         if card == 11 and all_points > 21:
             all_points -= 10
             print(f'because dealer has Ace and his value > 21 his new value is {all_points}')
-            time.sleep(1)
     print('checked dealer hand')
     return all_points
 
 def check_ace_player(player_hand , all_points):
-    for _ in player_hand:
-        if _ == 11 and all_points > 21:
+    for card in player_hand:
+        if card == 11 and all_points > 21:
             all_points -= 10
             print(f'because you have Ace and your value > 21 your new value is {all_points}')
-            return all_points
     print('checked player hand')
     return all_points
 
@@ -108,36 +99,29 @@ def menu_black():
             continue
         elif answer in ('add_funds' , '/add_funds','add funds','/add funds'):
             wallet_add()
-            time.sleep(1)
             continue
         elif answer in ('/payout' , 'payout'):
             wallet_payout()
-            time.sleep(1)
             continue
         elif answer in ('/start_game' ,'start_game','start game','/start game' , '/start' , 'start'):
             game_main()
-            time.sleep(1)
             continue
         elif answer in ('end' , '/end'):
             break
 
 def bet_amount(balance):
-    time.sleep(1)
     while True:
         try:
             bet = (int(input('how much do you want to bet?\n: ')))
         except ValueError:
             print('please enter a number')
-            time.sleep(1)
             continue
         if not 0 <= bet <= balance:
             print('invalid bet amount')
-            time.sleep(1)
             continue
         answer3 = str(input(f'your bet is {bet} , is that correct?\n: '))
         if answer3 in ('yes' , 'y'):
             print('let the game begin')
-            time.sleep(2)
             return bet
         else:
             continue
@@ -206,10 +190,8 @@ def wallet_payout():
 def game_round():
     deck = deck_refresh()
     dealer_cards , dealer_hand = dealer_turn(deck)
-    time.sleep(1)
     player_cards , player_hand = player_turn(deck)
     player_cards = check_ace_player(player_hand , player_cards)
-    time.sleep(1)
     dealer_cards = check_ace_dealer(dealer_hand , dealer_cards)
     while int(player_cards) < 21:
         answer1 = str(input('hit or stay : '))
@@ -218,20 +200,16 @@ def game_round():
             player_cards += new_card
             player_hand.append(new_card)
             print(f'your new cards value = {player_cards}')
-            time.sleep(1)
             player_cards = check_ace_player(player_hand , player_cards)
         elif answer1.lower() == 'stay':
             print(f'your final cards value = {player_cards}')
-            time.sleep(1)
             break
     while int(dealer_cards) < 17:
         print('dealers next card')
-        time.sleep(1)
         new_card = hit(deck)
         dealer_cards += new_card
         dealer_hand.append(new_card)
         print(f'dealers new cards value = {dealer_cards}')
-        time.sleep(1)
         dealer_cards = check_ace_dealer(dealer_hand , dealer_cards)
     outcome = check_game(player_cards , dealer_cards)
     if outcome == 'w':
